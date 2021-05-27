02bffbd3ab9f60675f87292b02a2a48ba9f1d9b8
blacklisten <756319278@qq.com>
2021-05-27 20:00:38 +0800

    fix: vite.config.js base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
 
 export default defineConfig({
+  base: '/hello-vue3/',
   plugins: [
     vue(),
     styleImport({
